bfec1087c16805db02a00982ddc76cb0205dbfc2
Fedora <miruthul.pc@gmail.com>
2024-05-23 18:38:03 +0530

    Added sounds and ricochet img

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 let bulletCurrentDirection;
 
-function stopGame(){
-  let oldElement = document.querySelector('.board');
+function stopGame() {
+  let oldElement = document.querySelector(".board");
   let newElement = oldElement.cloneNode(true);
   oldElement.parentNode.replaceChild(newElement, oldElement);
 }
@@ -10,29 +10,24 @@ function positionCoins(DB) {
   // This function positions the coins in the board with the locations given the positions data
   // structure
   for (let team in DB) {
-
     for (let coin in DB[team]) {
       const x = DB[team][`${coin}`].location?.at(0);
       const y = DB[team][`${coin}`].location?.at(2);
       if (x && y) {
         const el = document.getElementsByClassName(`index--${x}-${y}`);
-        const rotateFactor = DB[team][`${coin}`]?.rotation -1
-        console.log(rotateFactor)
+        const rotateFactor = DB[team][`${coin}`]?.rotation - 1;
+        console.log(rotateFactor);
         el[0].insertAdjacentHTML(
           "afterbegin",
           `<div class = 'coin-container'>
           <img src="assets/${coin}.svg" alt="" class = "${coin}" height="20px"
-          style = "transform: rotate(${(rotateFactor*90)}deg)"
+          style = "transform: rotate(${rotateFactor * 90}deg)"
           ></div>`
         );
         el[0].firstChild.style.backgroundColor = DB[team].teamColor;
         el[0].firstChild.style.opacity = 0.9;
-
       }
     }
-
-
-
   }
 }
 
@@ -43,7 +38,7 @@ function initializeBoard(DB) {
   location = document.createElement("div");
   location.className = "board";
   const parent = document.querySelector(".container");
-  parent.insertBefore(location,document.querySelector(".bottom-bar"));
+  parent.insertBefore(location, document.querySelector(".bottom-bar"));
   let boxHtml = " ";
   //Code for Generating boxHtml for all 8 blocks and adding it to HTML
   //(${i},${j})
@@ -77,7 +72,7 @@ function highlightValidMoves(moves) {
   });
 }
 
-function validMoves(loc, coin,pos) {
+function validMoves(loc, coin, pos) {
   // This function checks if the particular location has coins in it and returns the possible locations
   // the coins can move.
   const m = Number(loc.at(0));
@@ -111,7 +106,7 @@ function validMoves(loc, coin,pos) {
   return moves;
 }
 
-function moveCoin(movableLocations, DB, currentPlayer, coin, allBoxes,timer) {
+function moveCoin(movableLocations, DB, currentPlayer, coin, allBoxes, timer) {
   // This function is responsible for moving the coins and refreshing the screen after it
 
   movableLocations = movableLocations.map((loc) =>
@@ -128,15 +123,18 @@ function moveCoin(movableLocations, DB, currentPlayer, coin, allBoxes,timer) {
     initializeBoard(DB);
     stopGame();
     // console.log(`Timer for ${currentPlayer.teamName} is ended`);
-    clearInterval(timer)
+    clearInterval(timer);
     startGame(DB);
     startBullet(DB);
   };
   movableLocations.forEach((box) => box.addEventListener("click", coinClicked));
-  allBoxes.forEach((box) => {box.addEventListener("click", ()=>{
-    movableLocations.forEach((box) => {box.removeEventListener("click",coinClicked)})
-  });});
-
+  allBoxes.forEach((box) => {
+    box.addEventListener("click", () => {
+      movableLocations.forEach((box) => {
+        box.removeEventListener("click", coinClicked);
+      });
+    });
+  });
 }
 
 function startGame(DB) {
@@ -147,16 +145,16 @@ function startGame(DB) {
   const opponent = DB.find((team) => team.currentTeam === false);
   const locationsOfCurrentPlayer = [
     ...Object.values(currentPlayer)
-        .map((el) => el?.location)
-        .filter((el) => el !== undefined),
+      .map((el) => el?.location)
+      .filter((el) => el !== undefined),
   ];
   const locationsOfOpponentPlayer = [
     ...Object.values(opponent)
-        .map((el) => el?.location)
-        .filter((el) => el !== undefined),
+      .map((el) => el?.location)
+      .filter((el) => el !== undefined),
   ];
   // console.log(`Timer for ${currentPlayer.teamName} is started`);
-  const timer = runTimer(DB,currentPlayer.teamName);
+  const timer = runTimer(DB, currentPlayer.teamName);
   //******************************************************************//
 
   nodeListOfBoxes.forEach((box) => {
@@ -167,14 +165,14 @@ function startGame(DB) {
       //******************************************************************//
       if (locationsOfCurrentPlayer.includes(`${m},${n}`)) {
         const coin = box.firstChild.lastChild.getAttribute("class");
-        if (coin==="ricochet" || coin === "half-ricochet"){
+        if (coin === "ricochet" || coin === "half-ricochet") {
           rotateCoin();
         }
         // console.log(coin);
-        const loc = [m,n];
-        const pos = [...locationsOfCurrentPlayer, ...locationsOfOpponentPlayer]
+        const loc = [m, n];
+        const pos = [...locationsOfCurrentPlayer, ...locationsOfOpponentPlayer];
         const highlighted = validMoves(loc, coin, pos);
-        moveCoin(highlighted, DB, currentPlayer, coin, nodeListOfBoxes,timer);
+        moveCoin(highlighted, DB, currentPlayer, coin, nodeListOfBoxes, timer);
         // nodeListOfBoxes.forEach((box) =>
         //   box.removeEventListener("click", play)
         // );
@@ -186,8 +184,8 @@ function startGame(DB) {
         // );
         // return startGame(DB);
       }
-    }
-      box.addEventListener(`click`, play);
+    };
+    box.addEventListener(`click`, play);
     // clearInterval(timer)
   });
 }
@@ -202,31 +200,29 @@ function startBullet(DB) {
   const ball = document.createElement("div");
   ball.classList.add("ball");
   document.body.append(ball);
-  let x = box.x + (box.width-8)/2;
-  let y = box.y + (box.height-8)/2;
+  let x = box.x + (box.width - 8) / 2;
+  let y = box.y + (box.height - 8) / 2;
   // console.log(x, y);
-  if ((DB[i].teamName === "black")){
-    moveBullet(x,y,"down",DB,ball)
-  }
-  else if ((DB[i].teamName === "white")){
-    moveBullet(x,y,"up",DB,ball);
+  if (DB[i].teamName === "black") {
+    moveBullet(x, y, "down", DB, ball);
+  } else if (DB[i].teamName === "white") {
+    moveBullet(x, y, "up", DB, ball);
   }
 }
 
-function moveBullet(x,y,direction,DB,ball){
-
-  ball.style.top = y  + "px";
+function moveBullet(x, y, direction, DB, ball) {
+  ball.style.top = y + "px";
   ball.style.left = x + "px";
-  let bullet = new Audio('assets/sounds/bullet.wav');
+  let bullet = new Audio("assets/sounds/bullet.wav");
   bullet.play();
   let request;
   const checkBallWithinBoundary = function () {
     const board = document.querySelector(".board").getBoundingClientRect();
     if (
-        x <= board.x ||
-        y <= board.y ||
-        y >= board.height + board.y ||
-        x >= board.width + board.x
+      x <= board.x ||
+      y <= board.y ||
+      y >= board.height + board.y ||
+      x >= board.width + board.x
     ) {
       console.log("Bullet went out of box !!! 🔥🔥🔥");
       cancelAnimationFrame(request);
@@ -237,116 +233,117 @@ function moveBullet(x,y,direction,DB,ball){
     request = requestAnimationFrame(moveBulletDown);
     y += 5;
     checkBallWithinBoundary();
-    detectCollision(DB, x, y,request,ball);
+    detectCollision(DB, x, y, request, ball);
     ball.style.top = Number(y) + "px";
   };
   const moveBulletUp = function () {
     request = requestAnimationFrame(moveBulletUp);
     y -= 5;
     checkBallWithinBoundary();
-    detectCollision(DB, x, y,request,ball);
+    detectCollision(DB, x, y, request, ball);
     ball.style.top = Number(y) + "px";
   };
   const moveBulletRight = function () {
     request = requestAnimationFrame(moveBulletRight);
     x += 5;
     checkBallWithinBoundary();
-    detectCollision(DB, x, y,request,ball);
+    detectCollision(DB, x, y, request, ball);
     ball.style.left = Number(x) + "px";
   };
   const moveBulletLeft = function () {
     request = requestAnimationFrame(moveBulletLeft);
     x -= 5;
     checkBallWithinBoundary();
-    detectCollision(DB, x, y,request,ball);
+    detectCollision(DB, x, y, request, ball);
     ball.style.left = Number(x) + "px";
   };
   bulletCurrentDirection = direction;
 
-  if (direction === "down"){
+  if (direction === "down") {
     request = requestAnimationFrame(moveBulletDown);
 
-  y+=5;
-  }
-  else if (direction === "up"){
+    y += 5;
+  } else if (direction === "up") {
     request = requestAnimationFrame(moveBulletUp);
-    y-=5;
-  }
-  else if (direction === "right"){
+    y -= 5;
+  } else if (direction === "right") {
     request = requestAnimationFrame(moveBulletRight);
-    x+=5
-  }
-
-  else if (direction === "left") {
+    x += 5;
+  } else if (direction === "left") {
     request = requestAnimationFrame(moveBulletLeft);
-    x-=5
+    x -= 5;
   }
 }
 function getLocation(box) {
   const obj = box.getBoundingClientRect();
-  const xFactor = (obj.width-8)/2;
+  const xFactor = (obj.width - 8) / 2;
   const x0 = obj.x + xFactor;
-  const y0 = obj.y  + xFactor;
+  const y0 = obj.y + xFactor;
   const x1 = obj.x + obj.width - xFactor;
-  const y1 = obj.y +obj.height- xFactor;
+  const y1 = obj.y + obj.height - xFactor;
 
   return [x0, x1, y0, y1];
 }
 
-function detectCollision(DB, x, y,request,ball) {
-  checkCollision("titan", "white", x, y, DB,request,ball);
-  checkCollision("titan", "black", x, y, DB,request,ball);
-  checkCollision("ricochet", "white", x, y, DB,request,ball);
-  checkCollision("ricochet", "black", x, y, DB,request,ball);
-  checkCollision("half-ricochet", "white", x, y, DB,request,ball);
-  checkCollision("half-ricochet", "black", x, y, DB,request,ball);
-  checkCollision("tank", "white", x, y, DB,request,ball);
-  checkCollision("tank", "black", x, y, DB,request,ball);
+function detectCollision(DB, x, y, request, ball) {
+  checkCollision("titan", "white", x, y, DB, request, ball);
+  checkCollision("titan", "black", x, y, DB, request, ball);
+  checkCollision("ricochet", "white", x, y, DB, request, ball);
+  checkCollision("ricochet", "black", x, y, DB, request, ball);
+  checkCollision("half-ricochet", "white", x, y, DB, request, ball);
+  checkCollision("half-ricochet", "black", x, y, DB, request, ball);
+  checkCollision("tank", "white", x, y, DB, request, ball);
+  checkCollision("tank", "black", x, y, DB, request, ball);
 }
 
-function checkCollision(coin, teamName, x, y, DB,request,ball) {
+function checkCollision(coin, teamName, x, y, DB, request, ball) {
   const team = teamName === "black" ? 0 : 1;
-  const piece = getLocation(document.querySelector(
-      `.index--${DB[team][coin].
-      location.at(0)}-${DB[team][coin].location.at(2)}`));
+  const piece = getLocation(
+    document.querySelector(
+      `.index--${DB[team][coin].location.at(0)}-${DB[team][coin].location.at(
+        2
+      )}`
+    )
+  );
   // console.log(piece)
   if (x <= piece[1] && y <= piece[3] && x >= piece[0] && y >= piece[2]) {
     console.log(
       `Collided with ${coin} at ${teamName}!! at ${x} and ${y} 🎯🎯🎯`
     );
 
-  if (coin === "tank") {
-    // stop the bullet and disappear the bullet
-    cancelAnimationFrame(request);
-    ball.remove();
-  }
-  if (coin === "ricochet") {
-    const loc = DB[team].ricochet.location
-    //Change the direction of the bullet
-    const box = document
+    if (coin === "tank") {
+      // stop the bullet and disappear the bullet
+      cancelAnimationFrame(request);
+      ball.remove();
+    }
+    if (coin === "ricochet") {
+      const loc = DB[team].ricochet.location;
+      //Change the direction of the bullet
+      const box = document
         .querySelector(`.index--${loc.at(0)}-${loc.at(2)}`)
         .getBoundingClientRect();
-    let x = box.x + (box.width-8)/2;
-    let y = box.y + (box.height-8)/2;
-    cancelAnimationFrame(request);
-    if (DB[team].ricochet.rotation === 1) {
-      switch(bulletCurrentDirection){
-        case "down":
-          bulletCurrentDirection = "right";
-          break;
-        case "up":
-          bulletCurrentDirection = "left";
-          break;
-        case "right":
-          bulletCurrentDirection = "down";
-          break;
-        case "left":
+      let x = box.x + (box.width - 8) / 2;
+      let y = box.y + (box.height - 8) / 2;
+      cancelAnimationFrame(request);
+      if (DB[team].ricochet.rotation === 1) {
+        switch (bulletCurrentDirection) {
+          case "down":
+            bulletCurrentDirection = "right";
+            break;
+          case "up":
+            bulletCurrentDirection = "left";
+            break;
+          case "right":
+            bulletCurrentDirection = "down";
+            break;
+          case "left":
             bulletCurrentDirection = "up";
             break;
-      }}
+        }
+      }
 
       if (DB[team].ricochet.rotation === 2) {
-        switch(bulletCurrentDirection){
+        switch (bulletCurrentDirection) {
           case "down":
             bulletCurrentDirection = "left";
             break;
@@ -361,112 +358,111 @@ function checkCollision(coin, teamName, x, y, DB,request,ball) {
             break;
         }
       }
-      moveBullet(x,y,bulletCurrentDirection,DB,ball);
+      moveBullet(x, y, bulletCurrentDirection, DB, ball);
 
-    // moveBullet(x,y,"right",DB)
-
-  }
-  if (coin === "half-ricochet") {
-    const loc = DB[team]['half-ricochet'].location
-    const box = document
+      // moveBullet(x,y,"right",DB)
+    }
+    if (coin === "half-ricochet") {
+      const loc = DB[team]["half-ricochet"].location;
+      const box = document
         .querySelector(`.index--${loc.at(0)}-${loc.at(2)}`)
         .getBoundingClientRect();
-    let x = box.x + (box.width-8)/2;
-    let y = box.y + (box.height-8)/2;
-    cancelAnimationFrame(request);
-    if (DB[team]['half-ricochet'].rotation === 1) {
-      switch(bulletCurrentDirection){
-        case "down":
-          bulletCurrentDirection = "right";
-          break;
-        case "up":
-          //Destroy
-          break;
-        case "right":
-          //Destroy
-          break;
-        case "left":
-          bulletCurrentDirection = "up";
-          break;
-      }}
+      let x = box.x + (box.width - 8) / 2;
+      let y = box.y + (box.height - 8) / 2;
+      cancelAnimationFrame(request);
+      if (DB[team]["half-ricochet"].rotation === 1) {
+        switch (bulletCurrentDirection) {
+          case "down":
+            bulletCurrentDirection = "right";
+            break;
+          case "up":
+            //Destroy
+            break;
+          case "right":
+            //Destroy
+            break;
+          case "left":
+            bulletCurrentDirection = "up";
+            break;
+        }
+      }
 
-    if (DB[team]['half-ricochet'].rotation === 2) {
-      switch(bulletCurrentDirection){
-        case "down":
-          //Destroy
-          break;
-        case "up":
-          bulletCurrentDirection = "right"
-          break;
-        case "right":
-          //down
-          break;
-        case "left":
-          bulletCurrentDirection = "down";
-          break;
+      if (DB[team]["half-ricochet"].rotation === 2) {
+        switch (bulletCurrentDirection) {
+          case "down":
+            //Destroy
+            break;
+          case "up":
+            bulletCurrentDirection = "right";
+            break;
+          case "right":
+            //down
+            break;
+          case "left":
+            bulletCurrentDirection = "down";
+            break;
+        }
+      }
+      if (DB[team]["half-ricochet"].rotation === 3) {
+        switch (bulletCurrentDirection) {
+          case "down":
+            //Destroy
+            break;
+          case "up":
+            bulletCurrentDirection = "left";
+            break;
+          case "right":
+            bulletCurrentDirection = "down";
+            break;
+          case "left":
+            //Destroy
+            break;
+        }
+      }
+      if (DB[team]["half-ricochet"].rotation === 4) {
+        switch (bulletCurrentDirection) {
+          case "down":
+            bulletCurrentDirection = "left";
+            break;
+          case "up":
+            //Destroy
+            break;
+          case "right":
+            bulletCurrentDirection = "up";
+            break;
+          case "left":
+            //Destroy
+            break;
+        }
       }
     }
-    if (DB[team]['half-ricochet'].rotation === 3) {
-      switch(bulletCurrentDirection){
-        case "down":
-          //Destroy
-          break;
-        case "up":
-          bulletCurrentDirection = "left";
-          break;
-        case "right":
-          bulletCurrentDirection = "down";
-          break;
-        case "left":
-          //Destroy
-          break;
+    if (coin === "titan") {
+      ball.remove();
+      if (teamName === "black") {
+        console.log("White won the game");
+      } else if (teamName === "white") {
+        console.log("Black won the game");
       }
-    }
-    if (DB[team]['half-ricochet'].rotation === 4) {
-      switch(bulletCurrentDirection){
-        case "down":
-          bulletCurrentDirection = "left";
-          break;
-        case "up":
-          //Destroy
-          break;
-        case "right":
-          bulletCurrentDirection = "up";
-          break;
-        case "left":
-          //Destroy
-          break;
-      }
+      let gameOver = new Audio("assets/sounds/game-over-2.wav");
+      gameOver.play();
+      stopGame();
     }
   }
-  if (coin === "titan") {
-    ball.remove()
-    if (teamName === "black") {
-      console.log("White won the game")
-    }
-    else if (teamName === "white"){
-      console.log("Black won the game")
-    }
-    let gameOver = new Audio('assets/sounds/game-over-2.wav')
-    gameOver.play();
-    stopGame()
-  }}
 }
 
-function runTimer(DB,currentTeam){
-  currentTeam = (currentTeam==="black" ? 0 : 1);
-  let timerElement = document.querySelector(`.timer-${DB[currentTeam].teamName}`);
-  console.log(`${currentTeam} , ${DB[currentTeam].timeLeft}`)
+function runTimer(DB, currentTeam) {
+  currentTeam = currentTeam === "black" ? 0 : 1;
+  let timerElement = document.querySelector(
+    `.timer-${DB[currentTeam].teamName}`
+  );
+  console.log(`${currentTeam} , ${DB[currentTeam].timeLeft}`);
   return setInterval(() => {
     DB[currentTeam].timeLeft--;
     // console.log(`${currentTeam} , ${DB[currentTeam].timeLeft}`);
-    let seconds = DB[currentTeam].timeLeft%60;
-    let minutes = Math.floor(DB[currentTeam].timeLeft/60);
+    let seconds = DB[currentTeam].timeLeft % 60;
+    let minutes = Math.floor(DB[currentTeam].timeLeft / 60);
     timerElement.innerHTML = `${minutes}:${seconds}`;
   }, 1000);
-
 }
 
-function rotate(coin,DB,teamName){
-
-}
+function rotate(coin, DB, teamName) {}
